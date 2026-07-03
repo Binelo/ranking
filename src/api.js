d83cc790efb981d473a426114ee06e9efb32eb43
devDigitalMix <digitalmixdev@gmail.com>
2026-07-03 15:02:09 -0300

    api 3

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ranking-back.vercel.app/",
+  baseURL: "https://ranking-back.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
